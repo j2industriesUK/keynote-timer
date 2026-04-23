5d3c61a052853541be9a5cf14bb6dd09d8b9cbea
James Hickey <james.hickey@j2industries.com>
2026-04-23 19:28:32 +0100

    Initial Commit

---
+++ Keynote Timer/Keynote_TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Keynote_TimerApp.swift
+//  Keynote Timer
+//
+//  Created by James Wong-Hickey on 23/04/2026.
+//
+
+import SwiftUI
+
+@main
+struct Keynote_TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
